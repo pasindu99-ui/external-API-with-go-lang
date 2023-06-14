811ca0807b1286cfe7977b908eba00e9c20c3f2a
pasindu sandeepa <79826042+pasindu99-ui@users.noreply.github.com>
2023-06-14 19:19:21 +0530

    add struct

--- wetherApp/go.mod
+++ wetherApp/go.mod
@@ -2,8 +2,11 @@ module wetherApp
 
 go 1.20
 
+require github.com/labstack/echo v3.3.10+incompatible
+
 require (
-	github.com/labstack/echo v3.3.10+incompatible
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -14,4 +17,5 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- wetherApp/server.go
+++ wetherApp/server.go
@@ -1,35 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
 
-	e.GET("/users/:id", getUser)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:4200"},
+	}))
 
-	e.GET("/weather/", getWeatherDetails)
+	e.POST("/weather", getWeatherDetails)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
-func getUser(c echo.Context) error {
-	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+type City struct {
+	City_name string `json:"cityName"`
 }
 
 func getWeatherDetails(c echo.Context) error {
-	city_name := c.FormValue("location")
+
+	var requestData City
+
+	// Bind the JSON data from the request body into the requestData variable
+	if err := c.Bind(&requestData); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid JSON data")
+	}
+
+	// Access the values from the requestData variable
+	city_name := requestData.City_name
+	fmt.Println("city_name: ", city_name)
 	API_key := "db8e2e85e965d896159bf85f3880e393"
 	API_URL := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city_name, API_key)
 
@@ -41,6 +51,30 @@ func getWeatherDetails(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	return c.JSONBlob(http.StatusOK, body)
+	type WeatherData struct {
+		Main struct {
+			Temperature float64 `json:"temp"`     //struct field for the temp
+			Humidity    int     `json:"humidity"` //struct field for the humidity
+			Pressure    int     `json:"pressure"` //struct field for the pressure
+		} `json:"main"` //struct the field for the main weatherData with json tag
+		Name string `json:"name"` //field for the city name
+	}
+
+	var weatherData WeatherData                                //created the variable to get the unmarshaled weather data
+	if err := json.Unmarshal(body, &weatherData); err != nil { //unmarshal the json into the weatherdata variable
+		return err
+	}
+
+	temperatureCelcius := math.Round(weatherData.Main.Temperature - 273.15) // convert the kelvin to celcius
+
+	// Build a response map with the required fields
+	responseData := map[string]interface{}{
+		"City":        weatherData.Name,
+		"Humidity":    weatherData.Main.Humidity,
+		"Pressure":    weatherData.Main.Pressure,
+		"Temperature": temperatureCelcius,
+	}
+
+	return c.JSON(http.StatusOK, responseData)
 
 }
