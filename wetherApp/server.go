f64fcbf15b79b543028bcacab6b044867d78ab99
pasindu sandeepa <79826042+pasindu99-ui@users.noreply.github.com>
2023-06-17 19:05:00 +0530

    updated

--- wetherApp/server.go
+++ wetherApp/server.go
@@ -39,25 +39,35 @@ func getWeatherDetails(c echo.Context) error {
 
 	// Access the values from the requestData variable
 	city_name := requestData.City_name
-	fmt.Println("city_name: ", city_name)
 	API_key := "db8e2e85e965d896159bf85f3880e393"
 	API_URL := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city_name, API_key)
 
 	response, err := http.Get(API_URL)
 	if err != nil {
-		return err
+		return c.JSON(http.StatusOK, err)
+	} else if response.Status == "404 Not Found" {
+		fmt.Println("city not found")
+		return c.JSON(http.StatusOK, "city not found")
 	}
+
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return err
+		return c.JSON(http.StatusOK, err)
 	}
+
 	type WeatherData struct {
 		Main struct {
-			Temperature float64 `json:"temp"`     //struct field for the temp
-			Humidity    int     `json:"humidity"` //struct field for the humidity
-			Pressure    int     `json:"pressure"` //struct field for the pressure
-		} `json:"main"` //struct the field for the main weatherData with json tag
-		Name string `json:"name"` //field for the city name
+			Temperature float64 `json:"temp"`
+			Humidity    int     `json:"humidity"`
+			Pressure    int     `json:"pressure"`
+		} `json:"main"`
+		Name    string `json:"name"`
+		Weather []struct {
+			ID          int64  `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
 	}
 
 	var weatherData WeatherData                                //created the variable to get the unmarshaled weather data
@@ -70,6 +80,7 @@ func getWeatherDetails(c echo.Context) error {
 	// Build a response map with the required fields
 	responseData := map[string]interface{}{
 		"City":        weatherData.Name,
+		"Weather":     weatherData.Weather[0].Main,
 		"Humidity":    weatherData.Main.Humidity,
 		"Pressure":    weatherData.Main.Pressure,
 		"Temperature": temperatureCelcius,
